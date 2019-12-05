5182e1b041b2279dd0838968e78ffb52c7d34aa9
Yoav Vainrich <yoavain@gmail.com>
2019-12-05 23:51:32 +0200

    Replaced react-redux boilerplate with useState hook

--- src/renderer/components/Application.tsx
+++ src/renderer/components/Application.tsx
@@ -1,12 +1,11 @@
 import { hot } from "react-hot-loader/root";
 import * as React from "react";
-
-import CounterContainer from "../containers/CounterContainer";
+import Counter from "./Counter";
 
 const Application = () => (
     <div>
         Hello World from Electron!
-        <CounterContainer />
+        <Counter />
     </div>
 );
 
--- src/renderer/components/Counter.tsx
+++ src/renderer/components/Counter.tsx
@@ -1,30 +1,28 @@
 import * as React from "react";
+import { useState } from "react";
 
 require("./Counter.scss");
 const redCubeImg = require("./RedCube.jpg");
 
-export interface Props {
-    value: number;
+const Counter: React.FunctionComponent = () => {
+    const [counter, setCounter] = useState(0);
 
-    incrementValue: () => any;
-    decrementValue: () => any;
-}
-
-const Counter: React.FunctionComponent<Props> = ({ value, incrementValue, decrementValue }) => (
-    <div className="counter">
-        <p>
-            <img src={redCubeImg} />
-        </p>
-        <p id="counter-value">Current value: {value}</p>
-        <p>
-            <button id="increment" onClick={incrementValue}>
-                Increment
-            </button>
-            <button id="decrement" onClick={decrementValue}>
-                Decrement
-            </button>
-        </p>
-    </div>
-);
+    return (
+        <div className="counter">
+            <p>
+                <img src={redCubeImg} />
+            </p>
+            <p id="counter-value">Current value: {counter}</p>
+            <p>
+                <button id="increment" onClick={() => setCounter(counter + 1)}>
+                    Increment
+                </button>
+                <button id="decrement" onClick={() => setCounter(counter - 1)}>
+                    Decrement
+                </button>
+            </p>
+        </div>
+    );
+};
 
 export default Counter;
