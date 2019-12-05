62d9c619a9089c039a6485dc16597c04aa2de2eb
Yoav Vainrich <yoavain@gmail.com>
2019-12-06 00:31:25 +0200

    e2e tests

--- src/renderer/components/Application.tsx
+++ src/renderer/components/Application.tsx
@@ -1,8 +1,8 @@
-import { hot } from "react-hot-loader/root";
 import * as React from "react";
+import { hot } from "react-hot-loader/root";
 import Counter from "./Counter";
 
-const Application = () => (
+const Application: React.FunctionComponent = () => (
     <div>
         Hello World from Electron!
         <Counter />
