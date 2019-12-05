f903f8ee33443f1344a4a9b6c833fc1de6b447b3
Yoav Vainrich <yoavain@gmail.com>
2019-12-05 23:56:15 +0200

    Snapshot test

---
+++ test/components/Counter.spec.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as renderer from "react-test-renderer";
+
+import Counter from "../../src/renderer/components/Counter";
+
+describe("Counter component", () => {
+    it("renders correctly", () => {
+        const tree = renderer.create(<Counter />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
