21a45174f4e222d04c3f0ecd86bade840ca4e8f5
Yoav Vainrich <yoavain@gmail.com>
2020-02-20 22:17:35 +0200

    * Implement selected row using useState hook * Cleanup * Tests

--- src/renderer/components/FileListPanel.tsx
+++ src/renderer/components/FileListPanel.tsx
@@ -13,42 +13,42 @@ import { RowData } from "../../main/staticData";
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
-            width: '100%',
+            width: "100%",
             maxWidth: 360,
-            backgroundColor: theme.palette.background.paper,
+            backgroundColor: theme.palette.background.paper
         },
         inline: {
-            display: 'inline',
+            display: "inline"
         },
         large: {
             width: theme.spacing(6),
             height: theme.spacing(6)
         }
-    }),
+    })
 );
 
 export default function FileListPanel(props: { rows: RowData[] }) {
     const classes = useStyles();
+    const [selectedIndex, setSelectedIndex] = React.useState(-1);
+
+    const handleListItemClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>, index: number) => {
+        setSelectedIndex(index);
+    };
 
     return (
         <List className={classes.root}>
-            {
-                props.rows.map((row: RowData, index) => {
-                    return <React.Fragment>
-                        <ListItem alignItems="flex-start">
+            {props.rows.map((row: RowData, index) => {
+                return (
+                    <React.Fragment>
+                        <ListItem alignItems="flex-start" button selected={selectedIndex === index} onClick={event => handleListItemClick(event, index)}>
                             <ListItemAvatar>
-                                <Avatar alt={row.title} src={row.thumbnailLocation} variant="rounded" className={classes.large}/>
+                                <Avatar alt={row.title} src={row.thumbnailLocation} variant="rounded" className={classes.large} />
                             </ListItemAvatar>
                             <ListItemText
                                 primary={row.title}
                                 secondary={
                                     <React.Fragment>
-                                        <Typography
-                                            component="span"
-                                            variant="body2"
-                                            className={classes.inline}
-                                            color="textPrimary"
-                                        >
+                                        <Typography component="span" variant="body2" className={classes.inline} color="textPrimary">
                                             {row.artist}
                                         </Typography>
                                         {row.album} [{row.year}]
@@ -56,10 +56,10 @@ export default function FileListPanel(props: { rows: RowData[] }) {
                                 }
                             />
                         </ListItem>
-                        { index < props.rows.length && <Divider variant="inset" component="li"/> }
+                        {index < props.rows.length && <Divider variant="inset" component="li" />}
                     </React.Fragment>
-                })
-            }
+                );
+            })}
         </List>
     );
 }
