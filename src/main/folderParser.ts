7b2cc8bacf5b30b2e1715c4d9e634703b1415975
Yoav Vainrich <yoavain@gmail.com>
2020-03-10 00:37:52 +0200

    Working for MP3!!!

--- src/main/folderParser.ts
+++ src/main/folderParser.ts
@@ -1,6 +1,7 @@
 import { RowData } from "./staticData";
 import { Dirent, promises as fs } from "fs";
 import * as NodeID3 from "node-id3";
+import * as path from "path";
 
 enum SUPPORTED_FILE_TYPES {
     FLAC = ".flac",
@@ -8,30 +9,37 @@ enum SUPPORTED_FILE_TYPES {
 }
 
 const isRelevantFile = (file: Dirent): boolean => {
-    return file.isFile() && (file.name.endsWith(SUPPORTED_FILE_TYPES.FLAC) || file.name.endsWith(SUPPORTED_FILE_TYPES.MP3));
+    return file.isFile() && (file.name.toLowerCase().endsWith(SUPPORTED_FILE_TYPES.FLAC) || file.name.toLowerCase().endsWith(SUPPORTED_FILE_TYPES.MP3));
 };
 
 export const folderParser = async (dir: string): Promise<Array<RowData>> => {
     const files: Dirent[] = await fs.readdir(dir, { withFileTypes: true });
+    const relevantFiles: Dirent[] = files.filter((anyFile: Dirent) => isRelevantFile(anyFile));
     
-    return files.filter((anyFile) => isRelevantFile(anyFile)).map<RowData>((audioFile: Dirent) => {
-        const tags: NodeID3.Tags = NodeID3.read(audioFile.name);
+    const rowsData: RowData[] = [];
 
-        const rowData: RowData =  {
-            filename: audioFile.name,
-            title: tags.title,
-            artist: tags.artist,
-            album: tags.album,
-            year: tags.year,
-            track: tags.trackNumber,
-            hasLyrics: false,
-            lastModified: 0,
-            length: tags.length,
-            path: audioFile.name,
-            size: tags.size,
-            tag: tags.fileType
-        };
-
-        return rowData;
-    });
+    for (const audioFile of relevantFiles) {
+        const audioFileName = audioFile.name;
+        const audioFilePath = path.resolve(dir, audioFileName);
+        const fileContent: Buffer = await fs.readFile(audioFilePath);
+        const tags: NodeID3.Tags = NodeID3.read(fileContent);
+        if (tags) {
+            rowsData.push({
+                filename: audioFileName,
+                title: tags.title,
+                artist: tags.artist,
+                album: tags.album,
+                year: tags.year,
+                track: tags.trackNumber,
+                hasLyrics: false,
+                lastModified: 0,
+                length: tags.length,
+                path: audioFilePath,
+                size: tags.size,
+                tag: tags.fileType
+            });
+        }
+    }
+    
+    return rowsData;
 };
