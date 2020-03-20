84b24d9e74b2a07df2f2edaa6333d9b588a7f1e1
Yoav Vainrich <yoavain@gmail.com>
2020-03-20 07:37:29 +0200

    @testing-library/react * Simple snapshot tests

--- test/renderer/components/FileListPanel.test.tsx
+++ test/renderer/components/FileListPanel.test.tsx
@@ -1,20 +1,15 @@
 import * as React from "react";
-import { create, act, ReactTestRenderer } from "react-test-renderer";
 import FileListPanel from "../../../src/renderer/components/FileListPanel";
 import { rows } from "../../resources/staticData";
+import { render } from "@testing-library/react";
 
 describe("FileListPanel component", () => {
-    it("renders correctly", () => {
-        let root: ReactTestRenderer | undefined;
-        act(() => {
-            root = create(<FileListPanel rows={rows} onSelectItemClick={() => {}} selectedIndex={-1}/>);
-        });
-
-        expect(root?.toJSON()).toMatchSnapshot();
-
-        act(() => {
-            root?.update(<FileListPanel rows={rows} onSelectItemClick={() => {}} selectedIndex={0}/>);
-        });
-        expect(root?.toJSON()).toMatchSnapshot();
+    it("renders correctly - none selected", () => {
+        const fileListPanelNoneSelected = render(<FileListPanel rows={rows} onSelectItemClick={() => {}} selectedIndex={-1}/>);
+        expect(fileListPanelNoneSelected.baseElement).toMatchSnapshot();
+    });
+    it("renders correctly - with selected", () => {
+        const fileListPanelSelected = render(<FileListPanel rows={rows} onSelectItemClick={() => {}} selectedIndex={-1}/>);
+        expect(fileListPanelSelected.baseElement).toMatchSnapshot();
     });
 });
