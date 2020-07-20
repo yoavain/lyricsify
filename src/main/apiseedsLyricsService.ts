1d741adfcb641495d50ceaf3328c10dd1249d4db
Yoav Vainrich <yoavain@gmail.com>
2020-07-20 23:43:31 +0300

    * Started working on fetch lyrics from apiseeds

---
+++ src/main/apiseedsLyricsService.ts
@@ -0,0 +1,25 @@
+import { GetLyricsService, LyricsResult } from "./lyricsServiceIfc";
+import got from "got";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const tokens = require("../../tokens.json");
+
+const BASE_URL = "https://orion.apiseeds.com/api/music/lyric";
+const APISEEDS_KEY = process.env.APISEEDS_KEY || tokens.apiseeds;
+
+
+const buildUrl = (artist: string, track: string): string => {
+    const url = encodeURI(`${BASE_URL}/${artist}/${track}?apikey=${APISEEDS_KEY}`);
+    console.log(url);
+    return url;
+};
+
+export const getLyrics: GetLyricsService = async (artist: string, track: string): Promise<LyricsResult> => {
+    try {
+        return await got.get<LyricsResult>(buildUrl(artist, track)).json();
+    }
+    catch (e) {
+        // do nothing
+        console.log(e);
+    }
+};
---
+++ src/main/lyricsServiceIfc.ts
@@ -0,0 +1,24 @@
+export type LyricsResult = {
+    result: {
+        artist: {
+            name: string
+        },
+        track: {
+            name: string
+            text: string
+            lang: {
+                code: string
+                name: string
+            }
+        },
+        copyright: {
+            notice: string
+            artist: string
+            text: string
+        },
+        probability: number
+        similarity: number
+    }
+}
+
+export type GetLyricsService = (artist: string, track: string) => Promise<LyricsResult>
