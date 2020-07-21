667ca23a5669a8c3828ad9e7e3cdbd37ed04b643
Yoav Vainrich <yoavain@gmail.com>
2020-07-21 21:46:57 +0300

    rebase

--- src/renderer/components/Application.tsx
+++ src/renderer/components/Application.tsx
@@ -79,6 +79,7 @@ const Application = () => {
     };
 
     console.log("Rendering Application" + JSON.stringify({ theme: theme?.palette?.type, init, dir, selectedIndex, rows: rows.length }));
+    const selectedRow: RowData = rows[selectedIndex];
     return (
         <MuiThemeProvider theme={themeConfig}>
             <div>
@@ -92,12 +93,12 @@ const Application = () => {
                                 {selectedIndex > -1 &&
                                     <ReactPlayer
                                         className='react-player'
-                                        url={rows[selectedIndex].path}
+                                        url={selectedRow.path}
                                         controls={true}
                                         width='350px'
                                         height='35px'
                                         style={{ color: `${theme?.palette?.secondary}` }}
-                                        playIcon={rows[selectedIndex].thumbnail}
+                                        playIcon={selectedRow.thumbnail}
                                         playing={playing}
                                         onPlay={() => setPlaying(true)}
                                         onPause={() => setPlaying(false)}
@@ -116,7 +117,7 @@ const Application = () => {
                     </Grid>
                     <Grid item xs>
                         { selectedIndex >= 0 &&
-                        <LyricsCard title={rows[selectedIndex].title} artist={rows[selectedIndex].artist} lyrics={rows[selectedIndex].lyrics}/>
+                        <LyricsCard title={selectedRow.title} artist={selectedRow.artist} lyrics={selectedRow.lyrics}/>
                         }
                     </Grid>
                 </Grid>
